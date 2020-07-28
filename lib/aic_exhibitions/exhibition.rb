1a23197b1bd525d8796d0d46c9abb92072ac39fe
Thomas John Helin <thomashelin@yahoo.com>
2020-07-28 21:22:28 +0000

    finish descriptions reader method in Exhibitions class

--- lib/aic_exhibitions/exhibition.rb
+++ lib/aic_exhibitions/exhibition.rb
@@ -1,16 +1,23 @@
 class AicExhibitions::Exhibition
   attr_accessor :name
+  attr_writer :desription
   
   @@all = []
   
   def initialize(name)
     @name = name
     save
+    @descriptions = []
   end
   
   def self.all 
     AicExhibitions::Scraper.scrape_exhibitions if @@all.empty?
     @@all
+  end
+  
+  def descriptions
+    AicExhibitions::Scraper.scrape_events if @descriptions.empty?
+    @descriptions
   end
   
   def save
