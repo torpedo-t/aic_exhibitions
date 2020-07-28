4a14b6667b3185a61da4abed53e4b65da46475e4
Thomas John Helin <thomashelin@yahoo.com>
2020-07-28 19:12:34 +0000

    new class

--- lib/aic_exhibitions/exhibition.rb
+++ lib/aic_exhibitions/exhibition.rb
@@ -1,3 +1,15 @@
 class AicExhibitions::Exhibition
+  attr_accessor :description
+  
+  @@all = []
+  
+  def initialize()
+    
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
   
 end
--- lib/aic_exhibitions_environment.rb
+++ lib/aic_exhibitions_environment.rb
@@ -1,6 +1,7 @@
 # we add the require_relative's and pry within the environment file so they can all communicate with one another as well as pry
 require_relative "./aic_exhibitions/version"
 require_relative "./aic_exhibitions/cli"
+require_relative "./aic_exhibitions/exhibition"
 require 'pry'
 
 module AicExhibitions
