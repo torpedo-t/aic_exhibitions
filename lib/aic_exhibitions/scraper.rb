7c718f3c1b009999346fc330d24dc59310a78c6d
Thomas John Helin <thomashelin@yahoo.com>
2020-07-28 21:10:10 +0000

    webpage list

--- lib/aic_exhibitions/scraper.rb
+++ lib/aic_exhibitions/scraper.rb
@@ -10,6 +10,6 @@ class AicExhibitions::Scraper
   end
   
   def self.scrape_descriptions
-    site = [""
+    site = ["https://www.artic.edu/exhibitions/2939/el-greco-ambition-and-defiance", "https://www.artic.edu/exhibitions/9169/malangatana-mozambique-modern", "https://www.artic.edu/exhibitions/9560/toulouse-lautrec-and-the-celebrity-culture-of-paris", "https://www.artic.edu/exhibitions/9328/bauhaus-chicago-design-in-the-city", "https://www.artic.edu/exhibitions/9424/noda-tetsuya-my-life-in-print", "https://www.artic.edu/exhibitions/9562/intimate-modernity", "https://www.artic.edu/exhibitions/9249/photography-fine-art-material-meanings-selections-from-the-constance-r-caplan-collection", "https://www.artic.edu/exhibitions/9388/carissa-rodriguez-the-maid", "https://www.artic.edu/exhibitions/9167/signs-and-wonders-the-photographs-of-john-beasley-greene", "https://www.artic.edu/exhibitions/9107/pure-drawing-seven-centuries-of-art-from-the-gray-collection", "https://www.artic.edu/exhibitions/9389/louise-lawler", "https://www.artic.edu/exhibitions/9362/adornment-jewelry-of-south-asia-s-nomadic-cultures", "https://www.artic.edu/exhibitions/9318/postcommodity-with-each-incentive", "https://www.artic.edu/exhibitions/9208/a-new-view-of-african-art", "https://www.artic.edu/exhibitions/2353/the-deering-family-galleries-of-medieval-and-renaissance-art-arms-and-armor", "https://www.artic.edu/exhibitions/8965/of-gods-and-glamour-the-mary-and-michael-jaharis-galleries-of-greek-roman-and-byzantine-art", "https://www.artic.edu/exhibitions/8964/the-new-contemporary"]
   end
 end
